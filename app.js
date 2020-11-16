fd7c10336041df30b50a4b3bef43758731678aa7
zeddrix <37854513+zeddrix@users.noreply.github.com>
2020-11-16 11:38:29 +0800

    Able to populate item list with the items.

--- app.js
+++ app.js
@@ -16,6 +16,9 @@ const ItemCtrl = (() => {
   };
 
   return {
+    getItems: () => {
+      return data.items;
+    },
     logData: () => {
       return data;
     },
@@ -23,15 +26,35 @@ const ItemCtrl = (() => {
 })();
 
 const UICtrl = (() => {
-  return {};
+  const UISelectors = {
+    itemList: "#item-list",
+  };
+
+  return {
+    populateItemList: (items) => {
+      let html = "";
+
+      items.forEach((item) => {
+        html += `
+         <li class="collection-item" id="item-${item.id}">
+            <strong>${item.name}: </strong><em>${item.calories} Calories</em>
+            <a href="#" class="secondary-content"><i class="edit-item fa fa-pencil"></i></a>
+         </li>
+         `;
+      });
+
+      document.querySelector(UISelectors.itemList).innerHTML = html;
+    },
+  };
 })();
 
 const App = ((ItemCtrl, UICtrl) => {
-  console.log(ItemCtrl.logData());
-
   return {
     init: () => {
-      console.log("Initializing app...");
+      const items = ItemCtrl.getItems();
+      console.log(items);
+
+      UICtrl.populateItemList(items);
     },
   };
 })(ItemCtrl, UICtrl);
